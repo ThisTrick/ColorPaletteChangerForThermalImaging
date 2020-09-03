066cd2173d87a273091d96611ea8561d9abd450c
ThisTrick <way.trickster@gmail.com>
2020-09-03 20:18:04 +0300

    Создан пользовательский контроллер ColorPalette

---
+++ ColorPaletteChangerForThermalImaging/ColorPalette.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace ColorPaletteChangerForThermalImaging
+{
+    partial class ColorPalette
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label = new System.Windows.Forms.Label();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label.Location = new System.Drawing.Point(10, 270);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(60, 24);
+            this.label.TabIndex = 0;
+            this.label.Text = "label";
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.Color.White;
+            this.pictureBox.Location = new System.Drawing.Point(10, 10);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(105, 250);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // ColorPalette
+            // 
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.pictureBox);
+            this.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold);
+            this.Margin = new System.Windows.Forms.Padding(15);
+            this.Size = new System.Drawing.Size(125, 300);
+            this.FontChanged += new System.EventHandler(this.ColorPalette_FontChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.PictureBox pictureBox;
+    }
+}
---
+++ ColorPaletteChangerForThermalImaging/ColorPalette.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ColorPaletteChangerForThermalImaging
+{
+    public partial class ColorPalette : Panel
+    {
+
+        public string ColorPaletteName
+        {
+            get { return label.Text; }
+            set { label.Text = value; }
+        }
+
+        public Image Image
+        {
+            get { return pictureBox.Image; }
+            set { pictureBox.Image = value; }
+        }
+
+
+        public ColorPalette()
+        {
+            InitializeComponent();
+        }
+
+        private void ColorPalette_FontChanged(object sender, EventArgs e)
+        {
+            this.label.Font = this.Font;
+        }
+
+    }
+}
