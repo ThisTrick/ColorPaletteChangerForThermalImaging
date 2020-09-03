1c7d7eae181dafabafa9f9ac2adb673ce67f0c35
ThisTrick <way.trickster@gmail.com>
2020-09-03 22:15:34 +0300

    Добавлен обработчик ColorPalette_DoubleClick

--- ColorPaletteChangerForThermalImaging/SelectingColorPalette.cs
+++ ColorPaletteChangerForThermalImaging/SelectingColorPalette.cs
@@ -15,11 +15,19 @@ namespace ColorPaletteChangerForThermalImaging
 
     public partial class SelectingColorPalette : Form
     {
+        #region Перемещение формы
+
         [DllImport("user32.dll", EntryPoint = "ReleaseCapture")]
         extern static void ReleaseCapture();
         [DllImport("user32.dll", EntryPoint = "SendMessage")]
         extern static void SendMessage(System.IntPtr hwnd, int wmsg, int wparam, int lparam);
 
+        private void SelectingColorPalette_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+        #endregion
 
         private ColorPalette _selectedPalette;
         public SelectingColorPalette()
@@ -29,18 +37,32 @@ namespace ColorPaletteChangerForThermalImaging
 
         private void btnClose_Click(object sender, EventArgs e)
         {
+            this.DialogResult = DialogResult.Cancel;
             this.Close();
-        }
-
-        private void SelectingColorPalette_MouseDown(object sender, MouseEventArgs e)
-        {
-            ReleaseCapture();
-            SendMessage(this.Handle, 0x112, 0xf012, 0);
         }
 
         private void SelectingColorPalette_Load(object sender, EventArgs e)
         {
             InitColorPalettes("TestImage");
+        }
+
+        private void ColorPalette_Click(object sender, EventArgs e)
+        {
+            if (_selectedPalette != null)
+            {
+                _selectedPalette.BackColor = Color.Transparent;
+            }
+            var clickedPalette = sender as ColorPalette;
+            if (clickedPalette != null)
+            {
+                clickedPalette.BackColor = Color.FromArgb(15, 185, 177);
+                _selectedPalette = clickedPalette;
+            }
+        }
+        private void ColorPalette_DoubleClick(object sender, EventArgs e)
+        {
+            this.Tag = _selectedPalette.Image;
+            this.DialogResult = DialogResult.OK;
         }
 
         private void InitColorPalettes(string path)
@@ -75,9 +97,10 @@ namespace ColorPaletteChangerForThermalImaging
                 );
             colPalette.Location = newLocation;
             colPalette.Click += ColorPalette_Click;
+            colPalette.DoubleClick += ColorPalette_DoubleClick;
             return colPalette;
         }
-
+        
         private List<Bitmap> GetImages(string path)
         {
             var images = new List<Bitmap>();
@@ -87,23 +110,9 @@ namespace ColorPaletteChangerForThermalImaging
             foreach (var imgPath in imgPaths)
             {
                 var imgName = Path.GetFileNameWithoutExtension(imgPath);
-                images.Add(new Bitmap(imgPath) { Tag = imgName});
+                images.Add(new Bitmap(imgPath) { Tag = imgName });
             }
             return images;
-        }
-
-        private void ColorPalette_Click(object sender, EventArgs e)
-        {
-            if (_selectedPalette != null)
-            {
-                _selectedPalette.BackColor = Color.Transparent;
-            }
-            var clickedPalette = sender as ColorPalette;
-            if (clickedPalette != null)
-            {
-                clickedPalette.BackColor = Color.FromArgb(15, 185, 177);
-                _selectedPalette = clickedPalette;
-            }
         }
 
     }
