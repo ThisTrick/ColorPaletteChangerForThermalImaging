511340e0922d4a1b4fe44dfc09e60701b0816de1
ThisTrick <way.trickster@gmail.com>
2020-09-03 18:46:41 +0300

    Предварительные настройки MainForm

--- ColorPaletteChangerForThermalImaging/MainForm.Designer.cs
+++ ColorPaletteChangerForThermalImaging/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ColorPaletteChangerForThermalImaging
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,10 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 33F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(42)))), ((int)(((byte)(86)))));
+            this.ClientSize = new System.Drawing.Size(482, 474);
+            this.Font = new System.Drawing.Font("Rockwell", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(242)))), ((int)(((byte)(246)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "MainForm";
+            this.Text = "Color Palette Changer";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
