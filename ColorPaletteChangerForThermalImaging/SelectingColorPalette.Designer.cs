9a9cf41a5f481ab32147fe3ac5d97265cd31e8af
ThisTrick <way.trickster@gmail.com>
2020-09-03 19:44:01 +0300

    Создана Panel pColorPalettes

--- ColorPaletteChangerForThermalImaging/SelectingColorPalette.Designer.cs
+++ ColorPaletteChangerForThermalImaging/SelectingColorPalette.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.btnClose = new System.Windows.Forms.Button();
+            this.pColorPalettes = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // btnClose
@@ -37,7 +38,7 @@
             this.btnClose.FlatAppearance.BorderSize = 0;
             this.btnClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(66)))), ((int)(((byte)(250)))));
             this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnClose.Location = new System.Drawing.Point(522, 3);
+            this.btnClose.Location = new System.Drawing.Point(503, 3);
             this.btnClose.Margin = new System.Windows.Forms.Padding(0);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(45, 45);
@@ -46,17 +47,29 @@
             this.btnClose.UseVisualStyleBackColor = true;
             this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
+            // pColorPalettes
+            // 
+            this.pColorPalettes.AutoScroll = true;
+            this.pColorPalettes.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pColorPalettes.Location = new System.Drawing.Point(12, 51);
+            this.pColorPalettes.Name = "pColorPalettes";
+            this.pColorPalettes.Size = new System.Drawing.Size(526, 487);
+            this.pColorPalettes.TabIndex = 2;
+            // 
             // SelectingColorPalette
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 33F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(42)))), ((int)(((byte)(86)))));
-            this.ClientSize = new System.Drawing.Size(569, 532);
+            this.ClientSize = new System.Drawing.Size(550, 550);
+            this.Controls.Add(this.pColorPalettes);
             this.Controls.Add(this.btnClose);
             this.Font = new System.Drawing.Font("Rockwell", 16.2F, System.Drawing.FontStyle.Bold);
             this.ForeColor = System.Drawing.Color.White;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.MaximumSize = new System.Drawing.Size(550, 550);
+            this.MinimumSize = new System.Drawing.Size(550, 550);
             this.Name = "SelectingColorPalette";
             this.Text = "SelectingColorPalette";
             this.ResumeLayout(false);
@@ -66,5 +79,6 @@
         #endregion
 
         private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Panel pColorPalettes;
     }
 }
