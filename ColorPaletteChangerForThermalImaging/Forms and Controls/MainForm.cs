4578811a7fb5bfb62d498db59ae356b34f1c4fb8
ThisTrick <way.trickster@gmail.com>
2020-09-04 19:51:28 +0300

    Удалены using, что не используются

--- ColorPaletteChangerForThermalImaging/Forms and Controls/MainForm.cs
+++ ColorPaletteChangerForThermalImaging/Forms and Controls/MainForm.cs
@@ -1,13 +1,7 @@
 ﻿using ColorPaletteChangerForThermalImaging.Logic;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ColorPaletteChangerForThermalImaging
